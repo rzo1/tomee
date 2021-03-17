002f73dedc2f7bc55b08ac71be613f74632c2927
Richard Zowalla <rzo1@apache.org>
2021-03-17 20:42:51 +0100

    TOMEE-2977 - adds validation to PER_JVM mode,

--- container/openejb-junit5/src/test/java/org/apache/openejb/junit5/SingleAppComposerJVMTest.java
+++ container/openejb-junit5/src/test/java/org/apache/openejb/junit5/SingleAppComposerJVMTest.java
@@ -26,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 // just a manual test to check it works, can't be executed with the rest of the suite,
 // we could use a different surefire execution if we want to add it to the default run
+// should be executed in combination with SingleAppComposerTest
 //-Djunit.jupiter.testclass.order.default=org.apache.openejb.junit5.order.AppComposerTestClassOrderer -Dtomee.application-composer.application=org.apache.openejb.junit5.app.MyApp
 @RunWithApplicationComposer(mode = ExtensionMode.PER_JVM)
 public class SingleAppComposerJVMTest {
